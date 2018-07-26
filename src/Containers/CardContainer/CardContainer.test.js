f213074c88e7f7effba2145230b2333da1eb9154
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-26 11:56:14 -0600

    Add test for MSTP in CardContainer.js

--- src/Containers/CardContainer/CardContainer.test.js
+++ src/Containers/CardContainer/CardContainer.test.js
@@ -1,1 +1,25 @@
+import { mapStateToProps } from '../CardContainer/CardContainer';
+import { mockHouseInfo } from '../../mockData/mockHouseInfo';
+
+describe('CardContainer', () => {
+  describe('mapStateToProps', () => {
+    test('should return an object with the houses array', () => {
+      const mockState = {
+        housesInfo: mockHouseInfo,
+        testinfo: 'this is a test to maske sure we are only grabbing the part of state we want'
+      };
+
+      const expected = { 
+        housesInfo: mockHouseInfo
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+    
+  });
+  
+  
+});
 
