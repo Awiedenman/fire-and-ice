d3f4a1819bad48a47c18288a359afa51328c65cc
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-26 13:53:32 -0600

    Create apiCall for get request for swornMembers

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { requestSwornMemberInfo } from '../../apiCalls';
 
 
 
 export const Card = ({house}) => {
+  console.log(house);
   
+  const swornMemberId = (swornMembers) =>{
+    const seperateId = swornMembers.forEach( member => {
+      member.split('/').slice(5);
+      // const memberId = splitMemeber.slice(5);
+      // console.log(splitMember)
+    });
+    console.log(seperateId);
+    return seperateId;
+  };
+    // requestSwornMemberInfo(memberId);
+
   return (
-    <div className='Card'>
+    <div 
+      className='Card'
+      onClick={swornMemberId(house.swornMembers)}
+    >
       <p>name:{house.name}</p>
       <p>founded:{house.founded}</p>
       <p>seats:{house.seats}</p>
